110d06802d48b10694f5a3b3890d152da29016a5
Antony <aldana.antony@gmail.com>
2020-11-15 11:04:47 +0100

    SIGN UP

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -38,7 +38,7 @@ class Firebase {
 
   doSignOut = () => this.auth.signOut()
 
-  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email)
  
   doPasswordUpdate = password =>
       this.auth.currentUser.updatePassword(password)
