c06ee028655966622774d124713b66c64d4ec4a2
Antony <aldana.antony@gmail.com>
2020-11-21 11:30:09 +0100

    CREATE RECIPE FORM + STATE

--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -7,6 +7,30 @@ import { withAuthorization } from '../Session'
 import recettes from './recettes'
 
 class Recipe extends Component {
+
+  state = {
+    nom: '',
+    image: '',
+    ingredients: '',
+    instructions: ''
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const recette = { ...this.state }
+    this.props.ajouterRecette(recette)
+    this.resetForm(recette)
+  }
+
+  resetForm = () => {
+    this.setState({ nom: '', image: '', ingredients: '', instructions:'' })
+  }
+
   render () {
     const recipies = Object.keys(recettes)
       .map(key => <RecipeCard
@@ -17,6 +41,18 @@ class Recipe extends Component {
       <div>
         <h1>Vos recettes</h1>
         {recipies}
+
+        <div className='card'>
+          <form
+            className='admin-form ajouter-recette' 
+            onSubmit={this.handleSubmit}>
+            <input value={this.state.nom} type='text' name='nom' onChange={this.handleChange} placeholder='Nom de la recette' />
+            <input value={this.state.image} type='text' name='image' onChange={this.handleChange} placeholder={'Nom de l\'image'} />
+            <textarea value={this.state.ingredients} rows='3' name='ingredients' onChange={this.handleChange} placeholder='Nom des ingrédients' />
+            <textarea value={this.state.instructions} rows='15' name='instructions' onChange={this.handleChange} placeholder='Nom des instructions' />
+            <button type='submit'>Ajouter une recette</button>
+          </form>
+        </div>
       </div>
     )
   }
